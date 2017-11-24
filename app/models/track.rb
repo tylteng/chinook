478c32ae551267d80209556cb1ebf02903b68a87
tylteng <teng.ty4@gmail.com>
2017-11-24 10:26:03 -0500

    created track model methods for track length

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,26 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG = 360000
+
+  def self.short
+    shorter_than(SHORT)
+  end
+
+  def self.medium
+    longer_than_or_equeal_to(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    longer_than_or_equeal_to(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    where("milliseconds < ?", milliseconds)
+  end
+
+  def self.longer_than_or_equeal_to(milliseconds)
+    where("milliseconds >= ?", milliseconds)
+  end
+
 end
