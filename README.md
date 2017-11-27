7b282c488f1fbb78e2ec6fcd9e5d4fae1b596d2b
tylteng <teng.ty4@gmail.com>
2017-11-27 08:59:41 -0500

    README answers for intro to ActiveRecord

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-## README
+Question 1
+Artist.find_by(name: "Queen")
 
-[See instructions in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2038/latest)
+Question 2
+Track.where(media_type_id: '3').count
+
+Question 3
+Genre.where(name: 'Hip Hop/Rap')
+
+Question 4
+Track.where(genre_id: "17").count
+
+Question 5
+Track.sum(:milliseconds)
+
+Question 6
+Track.where(media_type_id: '3').maximum(:unit_price)
+
+Question 7
+Expensive = Track.where(media_type_id: '3').where(unit_price: '1.99')
+
+Expensive.find_each do |a|
+  puts a.name
+end
+
+Question 8
+
+
+Question 9
+Track.where(genre_id: '15').minimum(:unit_price)
+
+Question 10
--- app/models/track.rb
+++ app/models/track.rb
@@ -3,24 +3,17 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG = 360000
 
-  def self.short
-    shorter_than(SHORT)
-  end
-
-  def self.medium
-    longer_than_or_equeal_to(SHORT).shorter_than(LONG)
-  end
-
-  def self.long
-    longer_than_or_equeal_to(LONG)
-  end
-
-  def self.shorter_than(milliseconds)
-    where("milliseconds < ?", milliseconds)
-  end
-
-  def self.longer_than_or_equeal_to(milliseconds)
-    where("milliseconds >= ?", milliseconds)
-  end
+  # scope :short, -> { shorter_than(SHORT) }
+  # scope :medium, -> { longer_than_or_equeal_to(SHORT).shorter_than(LONG) }
+  # scope :long, -> { longer_than_or_equeal_to(LONG) }
+  # scope :starts_with, -> (char) { where('name ILIKE ?', "#{char}%") }
+  #
+  # scope :shorter_than, -> (milliseconds) {
+  #   if milliseconds && milliseconds > 0 where("milliseconds < ?", milliseconds)
+  # }
+  #
+  # def self.longer_than_or_equeal_to(milliseconds)
+  #   where("milliseconds >= ?", milliseconds)
+  # end
 
 end
